ef6196fb1b2f3ea210e2c6fcfb09228ff265603d
Rafael da Silva Almeida <eusou@rafaelalmeida.net>
2013-06-04 01:06:13 -0300

    Include Client class

--- nivel_2_banco/banco.py
+++ nivel_2_banco/banco.py
@@ -1,6 +1,11 @@
 import re
 import sys
 
+class Cliente():
+    def __init__(self, line):
+        self.t, self.d = line.split(" ")
+        self.t, self.d = int(self.t), int(self.d)
+
 def identifica_index_menor_espera( caixas ):
     return caixas.index(min(caixas))
 
@@ -13,15 +18,13 @@ def numero_clientes_insatisfeitos(entrada=sys.stdin):
     esperar = 0
 
     for x in xrange(clientes):
-        cliente = entrada.readline().split(" ")
-        chegou = int(cliente[0])
-        atendimento = int(cliente[1])
+        cliente = Cliente(entrada.readline())
 
         proximo_livre = identifica_index_menor_espera(quando_fica_livre)
 
-        espera = max([0, quando_fica_livre[proximo_livre] - chegou])
+        espera = max([0, quando_fica_livre[proximo_livre] - cliente.t])
 
-        quando_fica_livre[proximo_livre] = chegou + espera + atendimento
+        quando_fica_livre[proximo_livre] = cliente.t + espera + cliente.d
 
         if espera > limite_espera:
             esperar = esperar + 1
