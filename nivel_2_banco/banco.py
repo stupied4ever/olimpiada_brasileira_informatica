dc04b5f556d65c609d3ae4b332f123be9b6939de
Rafael da Silva Almeida <eusou@rafaelalmeida.net>
2013-06-04 01:28:28 -0300

    Include class ATM

--- nivel_2_banco/banco.py
+++ nivel_2_banco/banco.py
@@ -1,16 +1,26 @@
 import re
 import sys
+from operator import attrgetter
 
 class Cliente():
     def __init__(self, line):
         self.t, self.d = line.split(" ")
         self.t, self.d = int(self.t), int(self.d)
 
+class Caixa():
+    def __init__(self):
+        self.ocupado_ate = 0
+
+    @staticmethod
+    def proximo_livre(caixas):
+        return min(caixas,key=attrgetter('ocupado_ate'))
+
 def identifica_index_menor_espera( caixas ):
     return caixas.index(min(caixas))
 
 def numero_clientes_insatisfeitos(entrada=sys.stdin):
     banco = entrada.readline().split(" ")
+    caixas_abertos = [Caixa() for i in range(int(banco[0]))]
     quando_fica_livre = [0] * int(banco[0])
     clientes = int(banco[1])
 
@@ -20,11 +30,12 @@ def numero_clientes_insatisfeitos(entrada=sys.stdin):
     for x in xrange(clientes):
         cliente = Cliente(entrada.readline())
 
-        proximo_livre = identifica_index_menor_espera(quando_fica_livre)
+        # proximo_livre = identifica_index_menor_espera(quando_fica_livre)
+        proximo_caixa_livre = Caixa.proximo_livre(caixas_abertos)
 
-        espera = max([0, quando_fica_livre[proximo_livre] - cliente.t])
+        espera = max([0, proximo_caixa_livre.ocupado_ate - cliente.t])
 
-        quando_fica_livre[proximo_livre] = cliente.t + espera + cliente.d
+        proximo_caixa_livre.ocupado_ate = cliente.t + espera + cliente.d
 
         if espera > limite_espera:
             esperar = esperar + 1
