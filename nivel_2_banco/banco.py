69fe4f06ab179fb4e7b4b57e322f295090ce5441
Rafael da Silva Almeida <eusou@rafaelalmeida.net>
2013-06-04 02:52:33 -0300

    Refactor

--- nivel_2_banco/banco.py
+++ nivel_2_banco/banco.py
@@ -4,7 +4,7 @@ from operator import attrgetter
 
 class Cliente():
     def __init__(self, line):
-        self.t, self.d = line.split(" ")
+        self.t, self.d = line.split(' ')
         self.t, self.d = int(self.t), int(self.d)
 
 class Caixa():
@@ -19,20 +19,15 @@ class Caixa():
 
 class Banco():
     def __init__(self, entrada=sys.stdin):
-        total_caixas, total_clientes = entrada.readline().split(" ")
+        total_caixas, total_clientes = entrada.readline().split(' ')
         total_caixas, total_clientes = int(total_caixas), int(total_clientes)
 
-        self.caixas = [Caixa() for i in range(total_caixas)]
-        self.clientes = [Cliente(entrada.readline()) for linha in range(total_clientes)]
+        self.caixas = [Caixa() for _ in range(total_caixas)]
+        self.clientes = [Cliente(entrada.readline()) for _ in range(total_clientes)]
 
-    def numero_clientes_insatisfeitos(self):
-        esperar = 0
-
-        for cliente in self.clientes:
-            if self.__iniciar_atendimento(cliente) > 20:
-                esperar = esperar + 1
-
-        return esperar
+    def numero_clientes_insatisfeitos(self, espera_maxima=20):
+        esperas = map(self.__iniciar_atendimento, self.clientes)
+        return sum(espera > espera_maxima for espera in esperas)
 
     def __iniciar_atendimento(self, cliente):
         return self.__proximo_caixa_livre().atender(cliente)
