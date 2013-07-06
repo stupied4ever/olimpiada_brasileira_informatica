bb67995c59de2941046cc2ec9e6010a1275027a6
Rafael da Silva Almeida <eusou@rafaelalmeida.net>
2013-07-06 15:24:54 -0300

    If just need some words
    if just need some words on the start or on the end of string

--- ortografia/lib/auto_corretor.rb
+++ ortografia/lib/auto_corretor.rb
@@ -1,6 +1,21 @@
 class AutoCorretor
   def distancia(de, ate)
     return 0 if de == ate
-    return 1
+    return de.size  if ate.nil? || ate.empty?
+    return ate.size if de.nil?  || de.empty?
+    distancia = 0
+
+    if de.size != ate.size
+      menor, maior = [de,ate].sort{|a,b| a.size <=> b.size}
+      diferenca = maior.size - menor.size
+      if [maior[0...diferenca], menor].flatten.join == maior
+        distancia += diferenca
+      end
+      if [menor, maior.reverse[0...diferenca].reverse].flatten.join == maior
+        distancia += diferenca
+      end
+    end
+
+    return distancia
   end
 end
--- ortografia/spec/lib/auto_corretor_spec.rb
+++ ortografia/spec/lib/auto_corretor_spec.rb
@@ -7,16 +7,34 @@ describe 'AutoCorretor' do
     subject { auto_corretor.distancia(de, ate) }
     let(:de)  { 'rafael' }
 
-    context 'com segunda palavra faltando uma letra' do
+    context 'com duas palavras iguais' do
+      let(:ate) { 'rafael' }
+
+      it { should eq(0) }
+    end
+
+     context 'com segunda palavra faltando uma letra no fim' do
+       let(:ate) { 'rafae' }
+
+       it { should eq(1) }
+     end
+
+    context 'com segunda palavra faltando uma letra no inicio' do
       let(:ate) { 'afael' }
 
       it { should eq(1) }
     end
 
-    context 'com duas palavras iguais' do
-      let(:ate) { 'rafael' }
+    context 'com segunda palavra faltando duas letras no inicio' do
+      let(:ate) { 'fael' }
 
-      it { should eq(0) }
+      it { should eq(2) }
+    end
+
+    context 'com segunda palavra faltando todas as letras' do
+      let(:ate) { '' }
+
+      it { should eq(6) }
     end
   end
 end
